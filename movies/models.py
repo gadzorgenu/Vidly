85eee8740afcb6870b8bb79bc82591542baffc83
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-08-04 15:25:28 +0000

    update

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
@@ -14,3 +15,5 @@ class Movie(models.Model):
     number_in_stock = models.IntegerField()
     daily_rate = models.FloatField()
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    # passing a reference to the now method
+    date_created = models.DateTimeField(default=timezone.now)
