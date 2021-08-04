1cbbdfd154bfee1dcac253ec312d8c707f36c819
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-08-04 23:03:26 +0000

    update

--- movies/apps.py
+++ movies/apps.py
@@ -1,5 +1,17 @@
 from django.apps import AppConfig
+import sqlite3
+import json
+from pathlib import Path
 
 
 class MoviesConfig(AppConfig):
     name = 'movies'
+
+
+# genres = json.loads(Path('movies.json').read_text())
+
+# with sqlite3.connect('db.sqlite3') as conn:
+#     command = 'INSERT INTO movies_movie VALUES(?,?,?,?,?,?,?)'
+#     for genre in genres:
+#         conn.execute(command, tuple(genre.values()))
+#     conn.commit()
--- movies/models.py
+++ movies/models.py
@@ -1,5 +1,7 @@
 from django.utils import timezone
 from django.db import models
+import json
+from pathlib import Path
 
 # Create your models here.
 # inheriting the Genre class from the base class
@@ -7,6 +9,12 @@ from django.db import models
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
+
+# magic method ro represent the genre with it's name
+
+
+def __str__(self):
+    return self.name
 
 
 class Movie(models.Model):
@@ -17,3 +25,39 @@ class Movie(models.Model):
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     # passing a reference to the now method
     date_created = models.DateTimeField(default=timezone.now)
+
+
+# movies = [
+#     {
+#         'id': 1,
+#         'title': 'Terminator',
+#         'release_year': 1957,
+#         'number_in_stock': 3,
+#         'daily_rate': 1.57,
+#         'genre': 'Action',
+#         'date_created': '2021-08-04 21:20:45'
+#     },
+#     {
+#         'id': 2,
+#         'title': 'TerminatorX',
+#         'release_year': 1967,
+#         'number_in_stock': 4,
+#         'daily_rate': 2.57,
+#         'genre': 'Action',
+#         'date_created': '2021-08-04 21:20:45'
+#     },
+#     {
+#         'id': 3,
+#         'title': 'Terminator2',
+#         'release_year': 1987,
+#         'number_in_stock': 5,
+#         'daily_rate': 2.57,
+#         'genre': 'Action',
+#         'date_created': '2021-08-04 21:20:45'
+#     },
+# ]
+
+
+# data = json.dumps(movies)
+
+# Path('movies.json').write_text(data)
