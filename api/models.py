3b937281fe01ca72c3ad5421e00f7f35f7dc7ef6
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-08-05 13:24:56 +0000

    update

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+# Creatng apis
+from tastypie.resources import ModelResource
+from movies.models import Movie
+
 
 # Create your models here.
+class MovieResource(ModelResource):
+    class Meta:
+        queryset = Movie.objects.all()
+        resource_name = 'movies'
+        # not showing the date created field
+        excludes = ['date_created']
