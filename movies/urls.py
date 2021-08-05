e850bb6fbb1a1be5809807d6a00ff9517995061a
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-08-05 03:41:52 +0000

    update

--- movies/urls.py
+++ movies/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'movies'
 # mapping urls
 urlpatterns = [
     # path('') : representing the root of the app
-    path('', views.index, name='movies_index'),
+    path('', views.index, name='index'),
     # we use <paramName> to define url params
-    path('<int:movie_id>', views.detail, name='movies_detail')
+    path('<int:movie_id>', views.detail, name='detail')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 # Create your views here.
@@ -27,5 +27,7 @@ def index(request):
 
 
 def detail(request, movie_id):
-    movie = Movie.objects.get(pk=movie_id)
+
+    # movie = Movie.objects.get(pk=movie_id) OR pass the Movie class to the get_object_or_404 exception
+    movie = get_object_or_404(Movie, pk=movie_id)
     return render(request, template_name='movies/detail.html', context={'movie': movie})
